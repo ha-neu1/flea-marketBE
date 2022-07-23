0458f894451a1bfd737973b9818e9bb359d34bde
hongju22 <ghdwn14@naverc.om>
2022-07-23 19:24:43 +0900

    commit

--- src/main/java/com/fleamarket/demo/controller/UserController.java
+++ src/main/java/com/fleamarket/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.fleamarket.demo.controller;
 
 import com.fleamarket.demo.jwt.JwtTokenProvider;
 import com.fleamarket.demo.model.dto.LoginRequestDto;
+import com.fleamarket.demo.model.dto.UserDto;
 import com.fleamarket.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -34,4 +35,36 @@ public class UserController {
 //        return res.equals("") ? "회원가입 성공" : res;
 //    }
 
+
+    @PostMapping("/user/join/username")
+    public String registerUsername(@RequestBody UserDto userDto) {
+        String reu = UserService.registerUsername(userDto);
+        if (reu.equals("")) {
+            return "유저 이름 확인";
+        } else {
+            return reu;
+        }
+    }
+
+    @PostMapping("/user/join/nickname")
+    public String registerNickname(@RequestBody UserDto userDto) {
+        String ren = UserService.registerNickname(userDto);
+        if (ren.equals("")) {
+            return "닉네임 확인";
+        } else {
+            return ren;
+        }
+    }
+
+    @PostMapping("/user/join")
+    public String registerUser(@Valid @RequestBody UserDto requestDto) {
+        String res = UserService.registerUser(requestDto);
+        if (res.equals("")) {
+            return "회원가입 성공";
+        } else {
+            return res;
+        }
+    }
 }
+
+
--- src/main/java/com/fleamarket/demo/service/UserService.java
+++ src/main/java/com/fleamarket/demo/service/UserService.java
@@ -2,8 +2,11 @@ package com.fleamarket.demo.service;
 
 import com.fleamarket.demo.model.User;
 import com.fleamarket.demo.model.dto.LoginRequestDto;
+import com.fleamarket.demo.model.dto.ResultResponseDto;
+import com.fleamarket.demo.model.dto.UserDto;
 import com.fleamarket.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +30,33 @@ public class UserService {
             return false;
         }
         return true;
+    }
+        @Autowired
+        public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+            this.userRepository = userRepository;
+            this.passwordEncoder = passwordEncoder;
+        }
+
+        public void registerUser(UserDto requestDto){
+            String username =requestDto.getUsername();
+            String nickname = requestDto.getNickname();
+            String city = requestDto.getCity();
+            String enPassord = passwordEncoder.encode(requestDto.getPw());
+
+
+            UserDto user = new UserDto(username, city, nickname, enPassord);
+            User user1 =new User(user);
+            userRepository.save(user1);
+        }
+
+        public ResultResponseDto duplicateUsername(String username) {
+            return new ResultResponseDto(userRepository.existsByUsername(username));
+        }
+
+        public ResultResponseDto duplicatecNickname(String nickname) {
+            return new ResultResponseDto(userRepository.existsByNickname(nickname));
+        }
+
     }
 //    public String registerUser(SignupRequestDto requestDto) {
 //        String error = "";
@@ -63,4 +93,4 @@ public class UserService {
 //        userRepository.save(user);
 //        return error;
 //    }
-}
+
