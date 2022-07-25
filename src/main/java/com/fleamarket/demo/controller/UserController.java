8885dc2ea418666fa83bf737d2432345850f4728
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-07-25 17:38:41 +0900

    벡엔드 통합 수정 후 완료

--- src/main/java/com/fleamarket/demo/controller/UserController.java
+++ src/main/java/com/fleamarket/demo/controller/UserController.java
@@ -10,10 +10,7 @@ import com.fleamarket.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -25,29 +22,7 @@ public class UserController {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final UserService userService;
-    private Object ResponseEntity;
 
-    @PostMapping("/user/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto,
-                        HttpServletResponse response) {
-        if (this.userService.login(loginRequestDto)) {
-            String token = this.jwtTokenProvider.createToken(loginRequestDto.getUsername());
-            System.out.println("token = " + token);
-
-            // JWT 전송
-            response.setHeader("Authorization", token);
-
-            Cookie cookie = new Cookie("JWT", token);
-            cookie.setPath("/");
-            cookie.setHttpOnly(true);
-            cookie.setSecure(true);
-            response.addCookie(cookie);
-
-            return "로그인이 성공했습니다.";
-        } else {
-            return "닉네임 또는 패스워드를 확인해주세요";
-        }
-    }
     //회원가입
     @PostMapping("/user/join")
     public String registerUser(@RequestBody UserDto requestDto) {
@@ -69,9 +44,33 @@ public class UserController {
         return ResponseEntity.ok().body(stringMap);
     }
 
+    //회원 정보 조회
     @GetMapping("/user/info")
     public ResponseEntity<UserDto> myinfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok().body(userService.myinfo(userDetails));
+    }
+
+    //로그인
+    @PostMapping("/user/login")
+    public String login(@RequestBody LoginRequestDto loginRequestDto,
+                        HttpServletResponse response) {
+        if (this.userService.login(loginRequestDto)) {
+            String token = this.jwtTokenProvider.createToken(loginRequestDto.getUsername());
+            System.out.println("token = " + token);
+
+            // JWT 전송
+            response.setHeader("Authorization", token);
+
+            Cookie cookie = new Cookie("JWT", token);
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            response.addCookie(cookie);
+
+            return "로그인이 성공했습니다.";
+        } else {
+            return "닉네임 또는 패스워드를 확인해주세요";
+        }
     }
 //    @PostMapping({"/user/signup"})
 //    public String registerUser(@RequestBody @Valid SignupRequestDto requestDto) {
