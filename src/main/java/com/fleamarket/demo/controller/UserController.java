f29984a70f1161a7607e9d0941439184c453870e
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-07-25 16:59:48 +0900

    User update

--- src/main/java/com/fleamarket/demo/controller/UserController.java
+++ src/main/java/com/fleamarket/demo/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.fleamarket.demo.controller;
 
-<<<<<<<<< Temporary merge branch 1
-=========
 
->>>>>>>>> Temporary merge branch 2
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fleamarket.demo.model.dto.ResultResponseDto;
 import com.fleamarket.demo.model.dto.UserDto;
+import com.fleamarket.demo.repository.UserRepository;
+import com.fleamarket.demo.security.UserDetailsImpl;
 import com.fleamarket.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,25 +26,29 @@ public class UserController {
 
     //회원가입
     @PostMapping("/user/join")
-    public void registerUser(@RequestBody UserDto requestDto) {
+    public String registerUser(@RequestBody UserDto requestDto) {
         userService.registerUser(requestDto);
+        return "회원 가입이 완료되었습니다";
     }
 
     //아이디 중복 확인
     @PostMapping("/user/join/username")
-    public ResultResponseDto duplicateUsername(@PathVariable("username") String username) {
-        return userService.duplicateUsername(username);
+    public ResponseEntity<Map<String, String>> duplicateUsername(@RequestBody String username) throws JsonProcessingException {
+        Map<String, String> stringMap = userService.duplicateUsername(username);
+        return ResponseEntity.ok().body(stringMap);
     }
 
     //닉네임 중복 확인
     @PostMapping("/user/join/nickname")
-    public ResultResponseDto duplicateNickname(@PathVariable("nickname") String nickname) {
-        return userService.duplicatecNickname(nickname);
+    public ResponseEntity<Map<String, String>> duplicateNickname(@RequestBody String nickname) throws JsonProcessingException {
+        Map<String, String> stringMap = userService.duplicatecNickname(nickname);
+        return ResponseEntity.ok().body(stringMap);
     }
-<<<<<<<<< Temporary merge branch 1
-}
-=========
+
+    //회원 정보 조회
+    @GetMapping("/user/info")
+    public ResponseEntity<UserDto> myinfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(userService.myinfo(userDetails));
+    }
 }
 
-
->>>>>>>>> Temporary merge branch 2
--- src/main/java/com/fleamarket/demo/service/UserService.java
+++ src/main/java/com/fleamarket/demo/service/UserService.java
@@ -1,23 +1,24 @@
 package com.fleamarket.demo.service;
 
-<<<<<<<<< Temporary merge branch 1
 
-=========
->>>>>>>>> Temporary merge branch 2
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fleamarket.demo.model.User;
 import com.fleamarket.demo.model.dto.ResultResponseDto;
 import com.fleamarket.demo.model.dto.UserDto;
 import com.fleamarket.demo.repository.UserRepository;
+import com.fleamarket.demo.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-<<<<<<<<< Temporary merge branch 1
-=========
+import java.util.HashMap;
+import java.util.Map;
 
 
->>>>>>>>> Temporary merge branch 2
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -30,23 +31,49 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public ResultResponseDto registerUser(UserDto requestDto){
+    public void registerUser(UserDto requestDto){
         String username =requestDto.getUsername();
         String nickname = requestDto.getNickname();
         String city = requestDto.getCity();
         String enPassord = passwordEncoder.encode(requestDto.getPw());
 
-        UserDto user = new UserDto(username, city, nickname, enPassord);
+        if(!username.matches("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")) {
+            throw new IllegalArgumentException("email 형식에 맞지 않습니다.");
+        }
+        if (!nickname.matches("^[a-zA-Z0-9]{3,15}$")) {
+            throw new IllegalArgumentException("nickname 조건이 맞지 않습니다.");
+        }
+
+        UserDto user = new UserDto(username,nickname,city,enPassord);
         User user1 =new User(user);
         userRepository.save(user1);
+
     }
 
-    public ResultResponseDto duplicateUsername(String username) {
-        return new ResultResponseDto(userRepository.existsByUsername(username));
+    public HashMap<String, String> duplicateUsername(String username) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        boolean isusername = userRepository.existsByUsername(username);
+        if(isusername){
+            throw new IllegalArgumentException("아이디가 존재합니다.");
+        }
+        return (HashMap<String, String>) mapper.readValue(username, Map.class);
     }
 
-    public ResultResponseDto duplicatecNickname(String nickname) {
-        return new ResultResponseDto(userRepository.existsByNickname(nickname));
+    public HashMap<String, String> duplicatecNickname(String nickname) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        boolean isnickname = userRepository.existsByNickname(nickname);
+        if(isnickname){
+            throw new IllegalArgumentException("닉네임이 존재합니다.");
+        }
+        return (HashMap<String, String>) mapper.readValue(nickname, Map.class);
+    }
+
+
+    public UserDto myinfo(UserDetailsImpl userDetails) {
+        String username = userDetails.getUsername();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("내 정보가 없습니다."));
+        return new UserDto(user.getUsername(), user.getNickname(), user.getPw(), user.getCity());
     }
 
 }
